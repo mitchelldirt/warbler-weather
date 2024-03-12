2afa25933f5a977b6387a011c8ed3af7050d63ad
Mitchell Mudd <developer@mitchellmudd.dev>
2024-03-12 14:18:19 -0400

    Maybe it works now, but probably not

---
+++ config.js
@@ -0,0 +1,22 @@
+const setEnv = () => {
+  const fs = require("fs");
+  const writeFile = fs.writeFile;
+  const targetPath = "./src/environments/environment.ts";
+  require("dotenv").config({
+    path: "src/environments/.env",
+  });
+  // `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+    weatherApiKey: '${process.env[weatherApiKey]}',
+    production: true,
+    };
+    `;
+  writeFile(targetPath, envConfigFile, (err) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+  });
+};
+
+setEnv();
