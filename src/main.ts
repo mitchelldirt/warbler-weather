a26588002f4fbf9cd33a237d278a3262866585c7
Mitchell Mudd <developer@mitchellmudd.dev>
2024-03-13 22:12:45 -0400

    Trying to get my app to work again

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
-/// <reference types="@angular/localize" />
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { registerLocaleData } from '@angular/common';
-import localeEn from '@angular/common/locales/en';
-import localeFr from '@angular/common/locales/fr';
-
-registerLocaleData(localeFr, 'fr');
-registerLocaleData(localeEn, 'en-US');
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
